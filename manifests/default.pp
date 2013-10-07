5a5b1c5ee1dfa07e20ef173d503bc82c3050efc1
Gus Vargas <gvargas@hubspot.com>
2013-10-07 02:53:34 -0400

    Puppetize vagrant dev machine

---
+++ manifests/default.pp
@@ -0,0 +1,52 @@
+Exec {
+  path => ['/usr/bin', '/usr/local/bin'],
+}
+
+class nodejs-npm {
+  exec { "initial-update":
+    command => 'sudo apt-get update',
+  }
+
+  package { 'python-software-properties':
+    ensure => 'installed',
+    require => Exec['initial-update'],
+  }
+
+  exec { 'add-apt-repo':
+    command => 'add-apt-repository ppa:chris-lea/node.js',
+    require => Package['python-software-properties'],
+  }
+
+  exec { 'apt-update':
+    command => 'sudo apt-get update',
+    require => Exec['add-apt-repo'],
+  }
+
+  exec { 'install-nodejs':
+    command => 'sudo apt-get install -y --force-yes nodejs',
+    require => Exec['apt-update'],
+  }
+}
+
+class devtools {
+  require nodejs-npm
+
+  exec { 'install-brunch':
+    command => 'sudo npm install -g brunch',
+  }
+
+  exec { 'install-nodemon':
+    command => 'sudo npm install -g nodemon',
+  }
+}
+
+class startup {
+  require devtools
+
+  exec { 'start-server':
+    command => 'nodemon start.js &',
+    cwd => '/vagrant',
+  }
+}
+
+include nodejs-npm, devtools, startup
