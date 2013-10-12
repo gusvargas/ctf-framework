8e40bc9fc67223fcf990a39bc732b8fcd0191d51
Gus Vargas <gvargas@hubspot.com>
2013-10-12 19:18:35 -0400

    Setup MySQL tables and admin user

---
+++ db.sql
@@ -0,0 +1,33 @@
+USE ctf;
+
+CREATE TABLE IF NOT EXISTS Teams (
+    t_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32) UNIQUE,
+    password VARCHAR(40),
+    email VARCHAR(64),
+
+    PRIMARY KEY(t_id)
+);
+
+CREATE TABLE IF NOT EXISTS Challenges (
+    chal_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32) NOT NULL UNIQUE,
+    category VARCHAR(32) NOT NULL,
+    description VARCHAR(512) NULL,
+    points INT NOT NULL,
+    target VARCHAR(128),
+    flag VARCHAR(64),
+
+    PRIMARY KEY(chal_id)
+);
+
+CREATE TABLE IF NOT EXISTS Solutions (
+    t_id INT NOT NULL ,
+    chal_id INT NOT NULL ,
+    flag varchar(64) NULL,
+    time BIGINT NOT NULL,
+
+    PRIMARY KEY (t_id, chal_id, time),
+    FOREIGN KEY (t_id) REFERENCES Teams(t_id),
+    FOREIGN KEY (chal_id) REFERENCES Challenges(chal_id)
+);
--- puppet/manifests/db.pp
+++ puppet/manifests/db.pp
@@ -1,1 +1,21 @@
-include ::mysql::server
+$ROOT_PASSWORD = 'g0t_r00t'
+$ADMIN_PASSWORD = 'qwer1234'
+$USER_PASSWORD = ''
+
+class { 'mysql::server':
+  root_password           => $ROOT_PASSWORD,
+  remove_default_accounts => true,
+}
+
+mysql::db { 'ctf':
+  user      => 'admin',
+  password  => 'qwer1234',
+  host      => 'localhost',
+  grant     => ['SELECT', 'UPDATE', 'INSERT', 'DELETE'],
+}
+
+exec { 'create_tables':
+  command   => "mysql -u root --password='${ROOT_PASSWORD}' < /vagrant/db.sql",
+  path      => '/usr/bin/',
+  require   => Mysql_database['ctf'],
+}
