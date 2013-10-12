7818e617d496516acf5acb1951bad3989e5b3031
Gus Vargas <gvargas@hubspot.com>
2013-10-12 15:21:25 -0400

    Specify class wide path

--- puppet/modules/application/manifests/init.pp
+++ puppet/modules/application/manifests/init.pp
@@ -1,8 +1,8 @@
-Exec {
-  path => ['/usr/bin', '/usr/local/bin'],
-}
-
 class application {
+  Exec {
+    path => ['/usr/bin', '/usr/local/bin'],
+  }
+
   include application::nodejs_npm
   include application::devtools
   include application::startup
