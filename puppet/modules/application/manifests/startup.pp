e29f21ffcce1f8804f17a2703b0337aafac32ece
Gus Vargas <gvargas@hubspot.com>
2013-10-13 01:47:09 -0400

    Log brunch and nodemon output to a file for tailing

--- puppet/modules/application/manifests/startup.pp
+++ puppet/modules/application/manifests/startup.pp
@@ -2,8 +2,9 @@ class application::startup {
   require application::devtools
 
   exec { 'start-server':
-    command => 'nodemon -L start.js &',
+    command => 'nodemon -L start.js > /home/vagrant/dev.log &',
     cwd => '/vagrant',
+    creates => '/home/vagrant/dev.log'
   }
 }
 
