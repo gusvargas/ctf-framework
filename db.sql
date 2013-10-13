31d95f4e818122d207ea52faac576270626f83ee
Gus Vargas <gvargas@hubspot.com>
2013-10-13 12:56:44 -0400

    Use id as id

--- db.sql
+++ db.sql
@@ -1,16 +1,16 @@
 USE ctf;
 
 CREATE TABLE IF NOT EXISTS Teams (
-    t_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(32) UNIQUE,
     password VARCHAR(40),
     email VARCHAR(64),
 
-    PRIMARY KEY(t_id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS Challenges (
-    chal_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(32) NOT NULL UNIQUE,
     category VARCHAR(32) NOT NULL,
     description VARCHAR(512) NULL,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS Challenges (
     flag VARCHAR(64) NOT NULL,
     locked BOOLEAN NOT NULL,
 
-    PRIMARY KEY(chal_id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS Solutions (
@@ -29,6 +29,6 @@ CREATE TABLE IF NOT EXISTS Solutions (
     time BIGINT NOT NULL,
 
     PRIMARY KEY (t_id, chal_id, time),
-    FOREIGN KEY (t_id) REFERENCES Teams(t_id),
-    FOREIGN KEY (chal_id) REFERENCES Challenges(chal_id)
+    FOREIGN KEY (t_id) REFERENCES Teams(id),
+    FOREIGN KEY (chal_id) REFERENCES Challenges(id)
 );
