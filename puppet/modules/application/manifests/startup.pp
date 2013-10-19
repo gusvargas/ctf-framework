60afde8f1eee0458a8dd4e47abdbce986a8866ff
Gus Vargas <gvargas@hubspot.com>
2013-10-19 19:29:44 -0400

    Watch for changes in hbs files

--- puppet/modules/application/manifests/startup.pp
+++ puppet/modules/application/manifests/startup.pp
@@ -2,7 +2,7 @@ class application::startup {
   require application::devtools
 
   exec { 'start-server':
-    command => 'supervisor -e coffee ./start.js 2> /home/vagrant/dev.log &',
+    command => 'supervisor -e coffee,hbs ./start.js 2> /home/vagrant/dev.log &',
     cwd => '/vagrant',
     creates => '/home/vagrant/dev.log'
   }
