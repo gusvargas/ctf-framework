3fa2e49c1d171fed4d8959612ebc76ae43cdf250
Gus Vargas <gvargas@hubspot.com>
2013-11-07 22:50:40 -0500

    Build adminUI before starting app

--- puppet/modules/application/manifests/startup.pp
+++ puppet/modules/application/manifests/startup.pp
@@ -1,10 +1,16 @@
 class application::startup {
   require application::devtools
 
+  exec { 'build-adminUI':
+    command => 'brunch build',
+    cwd => '/vagrant/static/adminUI',
+  }
+
   exec { 'start-server':
     command => 'supervisor -e coffee,hbs ./start.js 2> /home/vagrant/dev.log &',
     cwd => '/vagrant',
-    creates => '/home/vagrant/dev.log'
+    creates => '/home/vagrant/dev.log',
+    require => Exec['build-adminUI'],
   }
 }
 
